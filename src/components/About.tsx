676522641080ea4d3cd40fa1f709a3b4236e5dd5
zephyrzhangz <zephyrzhangz@gmail.com>
2024-11-18 03:25:07 +1300

    Update ATC link

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -40,7 +40,7 @@ export const About = () => {
         Zephyr has performed in the Auckland Theatre Company iteration of{" "}
         <i>
           <a
-            href="https://archive.atc.co.nz/auckland-theatre-company/2022/scenes-from-a-yellow-peril/"
+            href="https://www.atc.co.nz/whats-on/past-seasons/past-2022-season/scenes-from-a-yellow-peril"
             target="_blank" // Open link in a new tab
             rel="noopener noreferrer" // Security measure
           >
