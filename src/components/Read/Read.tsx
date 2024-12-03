1a48cc56b46bfd64064262efdc499cfe0c4312a9
zephyrzhangz <zephyrzhangz@gmail.com>
2024-12-04 04:00:24 +1300

    Sort journals by date

--- src/components/Read/Read.tsx
+++ src/components/Read/Read.tsx
@@ -4,6 +4,7 @@ import {
   AccordionPanel,
   AccordionIcon,
   Box,
+  Text,
 } from "@chakra-ui/react";
 import { JournalLink } from "./JournalLink";
 import { DropdownJournal } from "./DropdownJournal";
@@ -11,116 +12,159 @@ import { DropdownJournal } from "./DropdownJournal";
 export const Read = () => {
   const accentColor = "#F56565";
 
-  // Updated type: use `entryType` to distinguish single/dropdown, and `type` for online/print
-  const journals: (
+  type JournalEntry =
     | {
         entryType: "single";
         name: string;
         url: string;
         type: "online" | "print";
+        date: string;
       }
     | {
         entryType: "dropdown";
         name: string;
-        entries: { title: string; url: string }[];
-      }
-  )[] = [
-    {
-      entryType: "dropdown",
-      name: "Sweet Mammalian",
-      entries: [
-        {
-          title: "Be gay, do crime",
-          url: "https://www.sweetmammalian.com/issue-eleven/zephyrzhang",
-        },
-        {
-          title: "sick leave",
-          url: "https://www.sweetmammalian.com/issue-nine/zephyr-zhang",
-        },
-      ],
-    },
-    {
-      entryType: "dropdown",
-      name: "The Spinoff",
-      entries: [
-        {
-          title: "the dancer",
-          url: "https://thespinoff.co.nz/books/11-10-2024/the-friday-poem-the-dancer-by-zephyr-zhang",
-        },
-        {
-          title: "you ask me if I still love you",
-          url: "https://thespinoff.co.nz/books/08-03-2024/the-friday-poem-you-ask-me-if-i-still-love-you-by-zephyr-zhang",
-        },
-      ],
-    },
+        entries: {
+          title: string;
+          url: string;
+          date: string;
+        }[];
+      };
 
-    {
-      entryType: "single",
-      name: "Frontier Poetry",
-      url: "https://www.frontierpoetry.com/2024/10/25/zephyr-zhang/",
-      type: "online",
-    },
-    {
-      entryType: "single",
-      name: "Cartridge Lit",
-      url: "https://cartridgelit.com/2024/10/23/animal-crossing/",
-      type: "online",
-    },
-    {
-      entryType: "single",
-      name: "Symposia",
-      url: "https://www.symposiamagazine.com/issue02/zephyr-zhang",
-      type: "online",
-    },
-    {
-      entryType: "single",
-      name: "Starling",
-      url: "https://www.starlingmag.com/issue-14/zephyr-zhang",
-      type: "online",
-    },
-    {
-      entryType: "single",
-      name: "Cordite",
-      url: "http://cordite.org.au/poetry/treat/cucumis-sativus-parvus/",
-      type: "online",
-    },
-    {
-      entryType: "single",
-      name: "Poetry Lab Shanghai",
-      url: "https://www.poetrylabshanghai.com/post/autumn24#:~:text=is%20a%20student.-,The%20air%2C%20thinning,-by%20Zephyr%20Zhang",
-      type: "online",
-    },
-    {
-      entryType: "single",
-      name: "bad apple",
-      url: "https://badapple.gay/2023/11/22/i-used-to-shop-at-newmarket-mall-until-i-realised-it-was-a-spaceship-run-by-evil-aliens-and-was-subsequently-abducted/",
-      type: "online",
-    },
-    {
-      entryType: "single",
-      name: "Rat World",
-      url: "https://www.ratworldmag.com/shop/p/rat-world-issue-seven",
-      type: "print",
-    },
-    {
-      entryType: "single",
-      name: "Landfall",
-      url: "https://www.otago.ac.nz/press/books/landfall-247-spring-2024",
-      type: "print",
-    },
-    {
-      entryType: "single",
-      name: "Rapture: An Anthology of Performance Poetry from Aotearoa New Zealand",
-      url: "https://aucklanduniversitypress.co.nz/rapture/",
-      type: "print",
-    },
-    {
-      entryType: "single",
-      name: "Mayhem",
-      url: "https://www.mayhemjournal.co.nz/shop/product/565007/mayhem-2021--issue-9/",
-      type: "print",
-    },
-  ];
+      const journals: JournalEntry[] = [
+        {
+          entryType: "single",
+          name: "Sweet Mammalian",
+          url: "https://www.sweetmammalian.com/issue-eleven/zephyrzhang",
+          type: "online",
+          date: "2024-11-09",
+        },
+        {
+          entryType: "single",
+          name: "Frontier Poetry", 
+          url: "https://www.frontierpoetry.com/2024/10/25/zephyr-zhang/",
+          type: "online",
+          date: "2024-10-25",
+        },
+        {
+          entryType: "single",
+          name: "Cartridge Lit",
+          url: "https://cartridgelit.com/2024/10/23/animal-crossing/",
+          type: "online", 
+          date: "2024-10-23",
+        },
+        {
+          entryType: "dropdown",
+          name: "The Spinoff",
+          entries: [
+            {
+              title: "the dancer",
+              url: "https://thespinoff.co.nz/books/11-10-2024/the-friday-poem-the-dancer-by-zephyr-zhang",
+              date: "2024-10-11",
+            },
+            {
+              title: "you ask me if I still love you",
+              url: "https://thespinoff.co.nz/books/08-03-2024/the-friday-poem-you-ask-me-if-i-still-love-you-by-zephyr-zhang",
+              date: "2024-03-08",
+            },
+          ],
+        },
+        {
+          entryType: "single",
+          name: "Poetry Lab Shanghai",
+          url: "https://www.poetrylabshanghai.com/post/autumn24#:~:text=is%20a%20student.-,The%20air%2C%20thinning,-by%20Zephyr%20Zhang",
+          type: "online",
+          date: "2024-09-16",
+        },
+        {
+          entryType: "single",
+          name: "Symposia",
+          url: "https://www.symposiamagazine.com/issue02/zephyr-zhang",
+          type: "online",
+          date: "2024-07-05",
+        },
+        {
+          entryType: "single",
+          name: "Rat World",
+          url: "https://www.ratworldmag.com/shop/p/rat-world-issue-seven",
+          type: "print",
+          date: "2024-07-04",
+        },
+        {
+          entryType: "single",
+          name: "Landfall",
+          url: "https://www.otago.ac.nz/press/books/landfall-247-spring-2024",
+          type: "print",
+          date: "2024-05-27",
+        },
+        {
+          entryType: "single",
+          name: "Cordite",
+          url: "http://cordite.org.au/poetry/treat/cucumis-sativus-parvus/",
+          type: "online",
+          date: "2024-07-05",
+        },
+        {
+          entryType: "single",
+          name: "bad apple",
+          url: "https://badapple.gay/2023/11/22/i-used-to-shop-at-newmarket-mall-until-i-realised-it-was-a-spaceship-run-by-evil-aliens-and-was-subsequently-abducted/",
+          type: "online",
+          date: "2023-11-22",
+        },
+        {
+          entryType: "single",
+          name: "Rapture: An Anthology of Performance Poetry from Aotearoa New Zealand",
+          url: "https://aucklanduniversitypress.co.nz/rapture/",
+          type: "print",
+          date: "2023-11-09",
+        },
+        {
+          entryType: "single",
+          name: "Sweet Mammalian",
+          url: "https://www.sweetmammalian.com/issue-nine/zephyr-zhang",
+          type: "online",
+          date: "2022-11-05",
+        },
+        {
+          entryType: "single",
+          name: "Starling",
+          url: "https://www.starlingmag.com/issue-14/zephyr-zhang",
+          type: "online",
+          date: "2022-02-13",
+        },
+        {
+          entryType: "single",
+          name: "Mayhem",
+          url: "https://www.mayhemjournal.co.nz/shop/product/565007/mayhem-2021--issue-9/",
+          type: "print",
+          date: "2021-12-10",
+        },
+       ];
+
+  function groupAndSortJournals(journals: JournalEntry[]) {
+    const grouped: Record<string, JournalEntry[]> = {};
+
+    journals.forEach((journal) => {
+      const date =
+        journal.entryType === "single" ? journal.date : journal.entries[0].date;
+      const year = date.split("-")[0];
+
+      if (!grouped[year]) grouped[year] = [];
+      grouped[year].push(journal);
+    });
+
+    const sortedYears = Object.keys(grouped).sort(
+      (a, b) => Number(b) - Number(a)
+    );
+
+    return sortedYears.map((year) => ({
+      year,
+      entries: grouped[year].sort((a, b) => {
+        const dateA = a.entryType === "single" ? a.date : a.entries[0].date;
+        const dateB = b.entryType === "single" ? b.date : b.entries[0].date;
+        return new Date(dateB).getTime() - new Date(dateA).getTime();
+      }),
+    }));
+  }
 
   return (
     <Box>
@@ -134,22 +178,29 @@ export const Read = () => {
           </AccordionButton>
         </h2>
         <AccordionPanel pb={4}>
-          {journals.map((journal, index) =>
-            journal.entryType === "dropdown" ? (
-              <DropdownJournal
-                key={index}
-                name={journal.name}
-                entries={journal.entries}
-              />
-            ) : (
-              <JournalLink
-                key={index}
-                name={journal.name}
-                url={journal.url}
-                type={journal.type}
-              />
-            )
-          )}
+          {groupAndSortJournals(journals).map(({ year, entries }) => (
+            <Box key={year} mb={4}>
+              <Text fontWeight="bold" mb={2}>
+                {year}
+              </Text>
+              {entries.map((journal, index) =>
+                journal.entryType === "dropdown" ? (
+                  <DropdownJournal
+                    key={index}
+                    name={journal.name}
+                    entries={journal.entries}
+                  />
+                ) : (
+                  <JournalLink
+                    key={index}
+                    name={journal.name}
+                    url={journal.url}
+                    type={journal.type}
+                  />
+                )
+              )}
+            </Box>
+          ))}
         </AccordionPanel>
       </AccordionItem>
     </Box>
