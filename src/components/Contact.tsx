7bacecfd0bac7f64a266549a5f4c8e26728f7453
Zephyr Zhang <zephyrzhangz@gmail.com>
2025-05-23 10:52:28 +1200

    remove github from contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -70,32 +70,32 @@ export const Contact = () => {
           direction={{ base: "column", sm: "row" }}
         >
           <Stack direction={"column"} spacing={6}>
-            <Button
-              rounded={"md"}
-              size={"md"}
-              fontWeight={"normal"}
-              color={"#F56565"}
-              px={9}
-              width="fit-content"
-              bg="transparent" // Transparent background
-              border="1px solid #F56565" // Red border
-              cursor={"pointer"}
-              as={"a"}
-              href="mailto:zephyrzhangz@gmail.com"
-              target="_blank" // Open link in a new tab
-              rel="noopener noreferrer" // Security measure
-              display={"inline-flex"}
-              alignItems={"center"}
-              justifyContent={"center"}
-              transition={"background 0.3s ease, border-color 0.3s ease"}
-              _hover={{
-                bg: useColorModeValue("blackAlpha.200", "transparent"), // Adjust hover background
-                border: `1px solid ${useColorModeValue("black", "white")}`,
-              }}
-            >
-              Email
-            </Button>
-            <Stack direction={"row"} spacing={4}>
+            <Stack direction={"row"} spacing={3}>
+              <Button
+                rounded={"md"}
+                size={"sd"}
+                fontWeight={"normal"}
+                color={"#F56565"}
+                px={5}
+                width="fit-content"
+                bg="transparent" // Transparent background
+                border="1px solid #F56565" // Red border
+                cursor={"pointer"}
+                as={"a"}
+                href="mailto:zephyrzhangz@gmail.com"
+                target="_blank" // Open link in a new tab
+                rel="noopener noreferrer" // Security measure
+                display={"inline-flex"}
+                alignItems={"center"}
+                justifyContent={"center"}
+                transition={"background 0.3s ease, border-color 0.3s ease"}
+                _hover={{
+                  bg: useColorModeValue("blackAlpha.200", "transparent"), // Adjust hover background
+                  border: `1px solid ${useColorModeValue("black", "white")}`,
+                }}
+              >
+                Email
+              </Button>
               <SocialButton
                 label={"Instagram"}
                 href={"https://instagram.com/klaxoon"}
