514557a3b8b526559db4d76e59f83ec20fcf383d
zephyrzhangz <zephyrzhangz@gmail.com>
2026-07-25 22:35:56 +1200

    Add smoke test and tidy

---
+++ src/components/App.test.tsx
@@ -0,0 +1,7 @@
+import { render } from "@testing-library/react";
+import { App } from "./App";
+
+test("renders the app without crashing", () => {
+  const { container } = render(<App />);
+  expect(container).not.toBeEmptyDOMElement();
+});
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,17 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import "@testing-library/jest-dom"
+// jest-dom adds custom matchers for asserting on DOM nodes, e.g.
+// expect(element).toBeInTheDocument()
+import "@testing-library/jest-dom";
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    addListener: () => {},
+    removeListener: () => {},
+    dispatchEvent: () => false,
+  }),
+});
