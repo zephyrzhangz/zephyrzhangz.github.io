fdecae40e35026b792802deff87eb4b0efafc949
zephyrzhangz <zephyrzhangz@gmail.com>
2026-07-26 22:06:38 +1200

    Update Nick's name

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -10,10 +10,26 @@ import { Box } from "@chakra-ui/react";
 import { ExternalLink } from "./ExternalLink";
 
 const events = [
-  { date: "12 Apr", label: "Reading with Nick Ashcroft", url: "https://theopenbook.co.nz/events/2026/3/16/its-what-he-would-have-wanted" },
-  { date: "11 Jun", label: "Yellow Lamp Vol. Basement", url: "https://basementtheatre.co.nz/blogs/whats-on/yellow-lamp-poetry-vol-basement" },
-  { date: "18 Jun", label: "Auckland Art Gallery Late Night", url: "https://www.undertheradar.co.nz/gig/102892/Gallery-Open-Late-Ersha-Island-Raging-Flowers-Dj-Edyonthebeat.utr" },
-  { date: "7 Jul", label: "AUP New Poets 12 Launch", url: "https://www.timeout.co.nz/upcoming-events/winterpoetry1" },
+  {
+    date: "12 Apr",
+    label: "Reading with Nick Ascroft",
+    url: "https://theopenbook.co.nz/events/2026/3/16/its-what-he-would-have-wanted",
+  },
+  {
+    date: "11 Jun",
+    label: "Yellow Lamp Vol. Basement",
+    url: "https://basementtheatre.co.nz/blogs/whats-on/yellow-lamp-poetry-vol-basement",
+  },
+  {
+    date: "18 Jun",
+    label: "Auckland Art Gallery Late Night",
+    url: "https://www.undertheradar.co.nz/gig/102892/Gallery-Open-Late-Ersha-Island-Raging-Flowers-Dj-Edyonthebeat.utr",
+  },
+  {
+    date: "7 Jul",
+    label: "AUP New Poets 12 Launch",
+    url: "https://www.timeout.co.nz/upcoming-events/winterpoetry1",
+  },
 ];
 
 export const Events = () => {
@@ -34,7 +50,8 @@ export const Events = () => {
         </Text>
         {events.map((event, i) => (
           <span key={i}>
-            {event.date} - <ExternalLink href={event.url}>{event.label}</ExternalLink>
+            {event.date} -{" "}
+            <ExternalLink href={event.url}>{event.label}</ExternalLink>
             {i < events.length - 1 && <br />}
           </span>
         ))}
